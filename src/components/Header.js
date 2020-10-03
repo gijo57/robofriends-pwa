d54ab720dead5e9ce7025f52b1489e23cbac1aa4
gijo57 <pekka-1992@hotmail.com>
2020-10-03 13:14:28 +0200

    PWA lighthouse optimization icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = React.memo(() => {
     return (
         <div>            
             <h1 className='f1'>RoboFriends</h1>
+            <CounterButton color='red' />
         </div>
     )   
 })
