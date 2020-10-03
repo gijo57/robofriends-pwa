00784937191ab99b55ccbf76d262912fcb475a1c
gijo57 <pekka-1992@hotmail.com>
2020-10-03 16:59:00 +0200

    PWA icons optimization

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = React.memo(() => {
     return (
         <div>            
             <h1 className='f1'>RoboFriends</h1>
-            <CounterButton color='red' />
+            <CounterButton color='red' style='margin 10px' />
         </div>
     )   
 })
